f770d9e9635a19a93949e4597f9aaf2d147d1e11
blane612 <86729003+blane612@users.noreply.github.com>
2021-07-01 17:32:32 +0000

    update

--- primary.py
+++ primary.py
@@ -11,7 +11,9 @@
 #       1 - create a print statement that prints the sum of two numbers
 #       2 - create a print statement that prints the sum of three numbers
 #       3 - create a print statement the prints the sum of two negative numbers
-
+print('the sum of 8 + 12 =', 8 + 12)
+print('the sum of 5 + 14 + -8 =', 5 + 14 + -8)
+print('the sum of -20 + -10 =', -20 + -10)
 
 
 # subtraction
@@ -19,7 +21,9 @@
 #       1 - create a print statement that prints the difference of two numbers
 #       2 - create a print statement that prints the difference of three numbers
 #       3 - create a print statement the prints the difference of two negative numbers
-
+print('the difference of 15 - 14 =', 15 - 14)
+print('the difference of 11 - 24 - 7 =', 11 - 24 - 7)
+print('the difference of -19 - -3 =', -19 - -3)
 
 
 # multiplication and true division
@@ -27,25 +31,26 @@
 #       1 - create a print statement the prints the product of two numbers
 #       2 - create a print statement that prints the dividend of two numbers
 #       3 - create a print statement that evaluates an operation using both multiplication and division
-
-
+print('the product of 7 * 4 =', 7 * 4)
+print('the dividend of 62 / 14 =', 62 / 14)
+print('(1 - 2) ** 1 / 2 - (17 ** (7 - 2)) =', (1 - 2) ** 1 / 2 - (17 ** (7 - 2)))
 
 # floor division
 #   instructions
 #       1 - using floor division, print the dividend of two numbers.
-
+print('the integer dividend of 100 / 10 =', 100 // 10)
 
 
 # exponentiation
 #   instructions
 #       1 - using exponentiation, print the power of two numbers
-
+print('the power of 3 ^ 90 =', 3 ** 90)
 
 
 # modulus
 #   instructions
 #       1 - using modulus, print the remainder of two numbers
-
+print('the remainder of 45 / 8 =', 45 % 8)
 
 # --------------- Section 2 --------------- #
 # ---------- String Concatenation --------- #
@@ -55,15 +60,18 @@
 #       1 - print the concatenation of your first and last name
 #       2 - print the concatenation of five animals you like
 #       3 - print the concatenation of each word in a phrase
-
-
+print('Brielle ' + 'Lane ')
+print('pandas ' + 'monkeys ' + 'peacocks ' + 'bunnies ' + 'seahorses ')
+print('pandas are really cute ' + 'monkeys are smart ' + 'peacocks are very pretty ' + 'bunnies eat funny ' + 'seahorses are colorful')
 
 # duplication
 #   instructions
-#       1 - print the duplpication of your first 5 times
+#       1 - print the duplication of your first name
 #       2 - print the duplication of a song you like 10 times
+print('Brielle' * 8)
+print('Streets' * 10)
 
-
-# concatenation and duplpication
+# concatenation and duplication
 #   instructions
 #       1 - print the concatenation of two strings duplicated 3 times each
+print('red '* 3 + '$ ' * 3)
